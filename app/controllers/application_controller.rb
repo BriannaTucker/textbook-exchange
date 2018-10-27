38a271f207da5542ccabeb7f84b04eb8065ee125
gregm700 <gregm700@verizon.net>
2018-10-27 19:18:10 +0000

    Trying again to get picture

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
+  
   def send_pic
     send_data open("#{Rails.root}/app/assets/images/book.jpg)", "rb") { |f| f.read}
   end
